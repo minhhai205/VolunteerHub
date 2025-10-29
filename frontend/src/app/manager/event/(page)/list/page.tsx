2d391a3fa4b6e40baea90292a53e6c2cd6ed5685
hienonichan <hvu6582@gmail.com>
2025-10-29 17:49:21 +0700

    manager event list, #38

--- frontend/src/app/manager/event/(page)/list/page.tsx
+++ frontend/src/app/manager/event/(page)/list/page.tsx
@@ -314,7 +314,7 @@ export default function EventsPage() {
                             stroke="currentColor"
                             strokeWidth="2"
                           >
-                                                   {" "}
+                            {" "}
                             <rect
                               x="3"
                               y="4"
@@ -357,14 +357,14 @@ export default function EventsPage() {
                                                    {" "}
                             <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
                                                    {" "}
-                            <circle cx="9" cy="7" r="4" />                  D  
-                                <path d="M23 21v-2a4 4 0 0 0-3-3.87" />         
-                                          <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                                 {" "}
+                            <circle cx="9" cy="7" r="4" />                     
+                              <path d="M23 21v-2a4 4 0 0 0-3-3.87" />           
+                                        <path d="M16 3.13a4 4 0 0 1 0 7.75" /> 
+                                               {" "}
                           </svg>
                                                {" "}
                           <span>
-                                            D        {" "}
+                                                   {" "}
                             <span className={styles.metaLabel}>Tham gia:</span>{" "}
                                                     {event.participants} người  
                                                {" "}
