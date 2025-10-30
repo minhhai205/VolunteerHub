506c479b8947c68f0e98a6b0ce6745d8575b1b99
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2025-10-30 21:10:40 +0700

    add ProtectedRoute to admin layout, manager layout for role-based access

---
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,60 @@
+// components/auth/ProtectedRoute.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getAccessToken } from "@/lib/token";
+
+interface ProtectedRouteProps {
+  allowedScopes?: string[]; // ["ADMIN"], ["MANAGER"], []
+  children: React.ReactNode;
+}
+
+export default function ProtectedRoute({
+  allowedScopes = [],
+  children,
+}: ProtectedRouteProps) {
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
+
+  useEffect(() => {
+    const token = getAccessToken();
+
+    console.log(token)
+    // Nếu không có token → redirect login
+    if (!token) {
+      router.replace("/auth/login");
+      return;
+    }
+
+    try {
+      // Decode payload
+      const payloadBase64 = token.split(".")[1];
+      const payload = JSON.parse(atob(payloadBase64));
+
+      const scopes: string[] = payload.scope?.split(" ") || [];
+
+      const roles = scopes.map((s: string) => s.replace(/^ROLE_/, ""));
+
+      // Kiểm tra quyền
+      const allowed =
+        allowedScopes.length === 0 ||
+        roles.some((r) => allowedScopes.includes(r));
+
+      if (!allowed) {
+        router.replace("/forbidden");
+        return;
+      }
+
+      // Nếu hợp lệ → render children
+      setAuthorized(true);
+    } catch (err) {
+      console.error("Token decode error:", err);
+      router.replace("/auth/login");
+    }
+  }, [allowedScopes, router]);
+
+  if (!authorized) return null;
+
+  return <>{children}</>;
+}
