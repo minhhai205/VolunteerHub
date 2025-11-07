d827d60da04fa55599d8f2443ed3047562a6b6bf
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2025-11-07 22:39:31 +0700

    Api get manager dashboard statistics

---
+++ backend/src/main/java/project/web/backend/controllers/ManagerDashboardController.java
@@ -0,0 +1,33 @@
+package project.web.backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.web.backend.dtos.response.ApiSuccessResponse;
+import project.web.backend.dtos.response.dashboard.ManagerDashboardResponseDTO;
+import project.web.backend.services.ManagerDashboardService;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@Validated
+@RequiredArgsConstructor
+@RequestMapping("/api/manager/dashboard")
+public class ManagerDashboardController {
+    private final ManagerDashboardService managerDashboardService;
+
+    @GetMapping("/statistics")
+    @PreAuthorize("hasRole('MANAGER')")
+    public ApiSuccessResponse<ManagerDashboardResponseDTO> getManagerDashboardStatistics() throws ExecutionException, InterruptedException {
+        return ApiSuccessResponse.<ManagerDashboardResponseDTO>builder()
+                .data(managerDashboardService.getManagerDashboardStatistics())
+                .status(HttpStatus.OK.value())
+                .message("Get manager dashboard statistics successfully")
+                .build();
+    }
+
+}
