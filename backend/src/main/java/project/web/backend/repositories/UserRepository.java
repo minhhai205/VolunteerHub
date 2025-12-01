e610a7a52161dea2db775c87432d33f7562574d7
hienonichan <hvu6582@gmail.com>
2025-12-01 22:05:21 +0700

    refactor get user, add search param

--- backend/src/main/java/project/web/backend/repositories/UserRepository.java
+++ backend/src/main/java/project/web/backend/repositories/UserRepository.java
@@ -13,47 +13,55 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-        @Query("""
-                        SELECT u FROM User u
-                        JOIN FETCH u.role r
-                        WHERE u.email=:email
-                        """)
-        Optional<User> findByEmail(@Param("email") String email);
+    @Query("""
+            SELECT u FROM User u
+            JOIN FETCH u.role r
+            WHERE u.email=:email
+            """)
+    Optional<User> findByEmail(@Param("email") String email);
 
-        @Query("""
-                        SELECT u FROM User u
-                        JOIN FETCH u.role r
-                        WHERE u.id=:id
-                        """)
-        Optional<User> findByIdWithRole(@Param("id") Long id);
+    @Query("""
+            SELECT u FROM User u
+            JOIN FETCH u.role r
+            WHERE u.id=:id
+            """)
+    Optional<User> findByIdWithRole(@Param("id") Long id);
 
-        @Query("""
-                        SELECT u FROM User u
-                        WHERE u.email=:email
-                        """)
-        Optional<User> findByEmailWithNoReferences(@Param("email") String email);
+    @Query("""
+            SELECT u FROM User u
+            WHERE u.email=:email
+            """)
+    Optional<User> findByEmailWithNoReferences(@Param("email") String email);
 
-        @Query("""
-                        SELECT u FROM User u
-                        JOIN FETCH u.role r
-                        WHERE
-                            r.name='USER'
-                                OR
-                            r.name='MANAGER'
-                        """)
-        Page<User> getAllUsersAndManagers(Pageable pageable);
+    @Query("""
+            SELECT u FROM User u
+            JOIN FETCH u.role r
+            WHERE
+                ( r.name='USER'
+                    OR
+                r.name='MANAGER' )
+                AND
+                ( u.fullName LIKE CONCAT('%', :search, '%')
+                    OR
+                  u.email LIKE CONCAT('%', :search, '%') )
+            """)
+    Page<User> getAllUsersAndManagers(Pageable pageable, @Param("search") String search);
 
-        @Query("""
-                        SELECT u FROM User u
-                        LEFT JOIN u.role r
-                        WHERE r.name='ADMIN'
-                        """)
-        List<User> findAllAdmin();
+    @Query("""
+            SELECT u FROM User u
+            LEFT JOIN u.role r
+            WHERE r.name='ADMIN'
+            """)
+    List<User> findAllAdmin();
 
-        @Query("""
-                        SELECT u FROM User u
-                        JOIN FETCH u.role r
-                        WHERE r.name = :role
-                        """)
-        Page<User> findAllByRoleName(@Param("role") String role, Pageable pageable);
+    @Query("""
+            SELECT u FROM User u
+            JOIN FETCH u.role r
+            WHERE r.name = :role
+             AND
+                ( u.fullName LIKE CONCAT('%', :search, '%')
+                    OR
+                  u.email LIKE CONCAT('%', :search, '%') )
+            """)
+    Page<User> findAllByRoleName(@Param("role") String role, Pageable pageable, @Param("search") String search);
 }
