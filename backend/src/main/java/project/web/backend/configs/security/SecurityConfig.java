c46a43951db40adfc257e308ab63d757f933822b
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2026-03-04 11:03:13 +0700

    Update SecurityConfig.java

--- backend/src/main/java/project/web/backend/configs/security/SecurityConfig.java
+++ backend/src/main/java/project/web/backend/configs/security/SecurityConfig.java
@@ -82,10 +82,13 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("*"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000"
+        ));
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);                
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
